c7bd741c14706adb7331358610503afa55a02625
skac112 <skac@poczta.fm>
2020-11-16 13:18:11 +0100

    methods Line.parallelBy() and Line.perpendicularBy()

--- shared/src/main/scala/com/github/skac112/vgutils/Line.scala
+++ shared/src/main/scala/com/github/skac112/vgutils/Line.scala
@@ -6,4 +6,16 @@ package com.github.skac112.vgutils
 case class Line(a: Double, b: Double, c: Double) {
   def isParallelTo(other: Line) = a*other.b - b*other.a != 0
 
+  /**
+   * Finds a line parallel to this line and passing through a given point
+   * @param pt
+   */
+  def parallelBy(pt: Point) = Line(a, b, -a*pt.x - b*pt.y)
+
+  /**
+   * Finds a line perpendicular to this line and passing through a given point
+   * @param pt
+   * @return
+   */
+  def perpendicularBy(pt: Point) = Line(b, -a, -b*pt.x + a*pt.y)
 }
